7c117e7197aa827597e00c8422e30cbb7b1f0e7d
Chiebuka Wellington <122882344+wellythemenace@users.noreply.github.com>
2026-03-04 00:17:51 -0330

    Add files via upload

---
+++ Work-In-Progress/BlokusDriver.java
@@ -0,0 +1,14 @@
+/*
+ *  Blokus Game Driver
+ *  Based on the template by mhatcher
+ *  @author Wellington
+ */
+public class BlokusDriver
+{
+	public static void main(String[] args)
+	{
+		// Create a new Blokus game window
+		// 20x20 board for Blokus
+		BlokusWindow game = new BlokusWindow(20, 20);
+	}
+}
