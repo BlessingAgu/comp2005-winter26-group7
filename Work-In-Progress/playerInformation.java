d3c113d5df3d2de40ddc066eae59bb02c2cb4d9c
sawshark12 <sawshark12@gmail.com>
2026-03-04 11:19:05 -0330

    Add files via upload

---
+++ Work-In-Progress/playerInformation.java
@@ -0,0 +1,32 @@
+package setUpGame;
+import java.awt.Color;
+
+public class playerInformation {
+	
+	public Player p1, p2, p3, p4;
+	
+	public playerInformation() {
+		
+		p1 = new Player(Color.RED, "Player1");
+		p2 = new Player(Color.RED, "Player2");
+		p3 = new Player(Color.RED, "Player3");
+		p4 = new Player(Color.RED, "Player4");
+		
+		
+		
+	}
+	
+	//public addPlayer() {
+	//	test.getNumberOfPlayers(); //replace test with varrible object in main for setup game
+	//	p1 = new Player(Red, Player1);	
+	//
+	//public changePlayerColour(Player p, Colour colour) {
+	//	p.setPlayerColour(colour);
+	//}
+	
+	//public changePlayerColour() {
+	//	
+	//}
+	
+
+}
---
+++ Work-In-Progress/setUpGame.java
@@ -0,0 +1,204 @@
+package setUpGame;
+import java.awt.*;
+import javax.swing.*;
+
+import javax.swing.JPanel;
+
+public class setUpGame extends JFrame{
+	
+	//player counter
+	private int numberOfPlayers;
+	
+	
+	public setUpGame(){
+		int numberOfPlayers = 0; // keeping player count tracked
+		
+		setTitle("Game Set Up");
+		setSize(800, 800);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setLayout(new GridBagLayout()); //setting Layout of the UI to be able to modifly to fit the size of players allowed in
+		GridBagConstraints gbc = new GridBagConstraints();
+		
+		
+		
+		//adding the Back and the start game buttons 
+		JButton back = new JButton();
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		add(back);
+		
+		back.addActionListener(e -> {
+			
+			//mainMenu(); meant to return players to the main menu once it is implimented 
+			
+			
+		});
+		
+		
+		JButton start = new JButton();
+		gbc.gridx = 1;
+		gbc.gridy = 0;
+		add(start);
+		
+		start.addActionListener(e -> {
+			
+			//gameboard(); //tie into the game board 
+			
+			
+		});
+		
+		JButton player1Button = new JButton();
+		gbc.gridx = 0;
+		gbc.gridy = 1;
+		add(player1Button);
+		
+		JButton player2Button = new JButton();
+		gbc.gridx = 1;
+		gbc.gridy = 1;
+		add(player2Button);
+		player2Button.setVisible(false);
+		
+		JButton player3Button = new JButton();
+		gbc.gridx = 0;
+		gbc.gridy = 3;
+		add(player3Button);
+		player3Button.setVisible(false);
+		
+		JButton player4Button = new JButton();
+		gbc.gridx = 1;
+		gbc.gridy = 3;
+		add(player4Button);
+		player4Button.setVisible(false);
+		
+		JButton addPlayerButton = new JButton();
+		gbc.gridx = 0;
+		gbc.gridy = 4;
+		add(addPlayerButton);
+		
+		JButton removePlayerButton = new JButton();
+		gbc.gridx = 1;
+		gbc.gridy = 4;
+		add(removePlayerButton);
+		
+		JButton optionsButton = new JButton();
+		gbc.gridx = 0;
+		gbc.gridy = 5;
+		add(optionsButton);
+		
+		optionsButton.addActionListener(e -> {
+			//OptionsMenu();
+			
+		});
+		
+        setLocationRelativeTo(null); // puts it right in the middle of the screen
+        setVisible(true);
+		
+		
+	}
+	
+	//public void addPlayer(){
+	//	if (numberOfPlayers == 4) {
+	//		tooManyPlayers();
+	//	}
+	//	else {
+	//		if (numberOfPlayers == 0) {
+	//			Player p1 = new Player();
+	//			
+	//			}
+	//		else if (numberOfPlayers == 1) {
+	//			Player p2 = new Player();
+	//		}
+	//		else if (numberOfPlayers == 2) {
+	//			Player p3 = new Player();
+	//		}
+		//	else if (numberOfPlayers == 3) {
+		//		Player p4 = new Player();
+		//	}
+	//	}
+		
+	//}
+	
+	public void changePlayerColor() {
+		
+		JFrame colorPicker = new JFrame("color selection");
+		colorPicker.setSize(400, 200);
+		colorPicker.setLayout(new BoxLayout(colorPicker.getContentPane(), BoxLayout.Y_AXIS));
+		
+		//creating the menu to pick color from
+		JButton color1B = new JButton("");
+		color1B.setBackground(Color.RED);
+		
+		color1B.addActionListener(e -> {
+			//changePlayerColour()
+			
+		});
+		
+		JButton color2B = new JButton("");
+		color2B.setBackground(Color.GREEN);
+		
+		color2B.addActionListener(e -> {
+			//changePlayerColour()
+			
+		});
+		
+		JButton color3B = new JButton("");
+		color3B.setBackground(Color.BLUE);
+		
+		color3B.addActionListener(e -> {
+			//changePlayerColour()
+			
+		});
+		
+		JButton color4B = new JButton("");
+		color4B.setBackground(Color.YELLOW);
+		
+		color4B.addActionListener(e -> {
+			//changePlayerColour()
+			
+		});
+		
+		
+	}
+	
+	public void playerTypeMenu() {
+		JFrame playerType = new JFrame("Chose Player type"); // pops up a menu to chose a player type
+		playerType.setSize(600, 750);
+		playerType.setLayout(new BoxLayout(playerType.getContentPane(), BoxLayout.Y_AXIS));
+		
+		JButton compPlayer = new JButton("Computer Player");
+		compPlayer.setFont(new Font("Arial", Font.PLAIN, 18));
+		compPlayer.setAlignmentX(Component.CENTER_ALIGNMENT);
+		
+		compPlayer.addActionListener(e -> {
+			
+			//playerInformation.addNewComputerPlayer() //ment for later itoration when computer player is fucntional 
+			
+			
+		});
+		
+		JButton humanPlayer = new JButton("Human Player");
+		humanPlayer.setFont(new Font("Arial", Font.PLAIN, 18));
+		humanPlayer.setAlignmentX(Component.CENTER_ALIGNMENT);
+		
+		humanPlayer.addActionListener(e -> {
+			
+			//playerInformation.addPlayer();
+			//changePlayerColor();
+			
+		});
+		
+	}
+	
+	public void tooManyPlayers() { //convert to it's own class later for error handling
+		
+	}	
+	
+	public int getNumberOfPlayers() { //getter for number of players
+		return numberOfPlayers;
+	}
+
+	public static void main(String[] args) {
+		setUpGame test = new setUpGame();
+	};
+
+}
